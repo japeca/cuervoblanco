41ad03549f99de27a1aed6b7f6391471addf318d
japeca <j.perezcaminal@gmail.com>
2024-11-21 20:06:49 +0100

    Se añade respawn sencillo
    El desarrollador coloca un respawn y el personaje aparece en él. Hay que colocar varios (en lugares estáticos) y que se activen según pase por ahi.

--- CuervoBlancoUnityGame/Assets/Scripts/CameraFollower.cs
+++ CuervoBlancoUnityGame/Assets/Scripts/CameraFollower.cs
@@ -11,12 +11,37 @@ public class CameraFollower : MonoBehaviour
     public float speedCam = 0.025f;
     public Vector3 desplazamiento;
     public Transform Objetivo;
+    public Transform respawnPoint;
+    public float limiteCaida = -10f;
 
 
     private void LateUpdate()
     {
+        if (Objetivo.position.y < limiteCaida)
+        {
+            RespawnPersonaje();
+            return;
+        }
+
         Vector3 posicionDeseada = Objetivo.position + desplazamiento;
         Vector3 posicionSuavizada = Vector3.Lerp(transform.position, posicionDeseada, speedCam);
         transform.position = posicionSuavizada;
     }
+
+    private void RespawnPersonaje()
+    {
+        // Mueve el personaje al punto de reaparicion.
+        Objetivo.position = respawnPoint.position;
+
+       
+        Rigidbody2D rb = Objetivo.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+        //Ajustar la camara para posicionarla directamente sobre el personaje.
+        transform.position = respawnPoint.position + desplazamiento;
+    }
+
 }
